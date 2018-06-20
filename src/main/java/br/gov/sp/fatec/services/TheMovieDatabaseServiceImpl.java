9750c08c3c499cca5c6d90f25b430715628c4ae9
Felipe Koblinger <koblinger@live.com>
2018-06-20 00:14:40 -0300

    Update Spring Framework from 5.0.5 to 5.0.6 and Spring Framework Data from 2.0.6 to 2.0.8. Change TheMovieDatabase API Key to environment variable. Invalidate old API key.

--- src/main/java/br/gov/sp/fatec/services/TheMovieDatabaseServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/services/TheMovieDatabaseServiceImpl.java
@@ -3,22 +3,26 @@ package br.gov.sp.fatec.services;
 import br.gov.sp.fatec.utils.JsonIO;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("theMovieDatabaseService")
 public class TheMovieDatabaseServiceImpl implements TheMovieDatabaseService {
+
+    @Value("${THE_MOVIE_DB_API_KEY}")
+    private String apiKey;
+
     @Override
     public JsonNode getMovieById(String id) {
         String url = String.format("https://api.themoviedb.org" +
-                "/3/movie/%s?api_key=21bf4ea44600c1fd5d93194cbfd6fd71", id);
-
+                "/3/movie/%s?api_key=%s", id, this.apiKey);
         return new JsonIO().load(url);
     }
 
     @Override
     public JsonNode searchMoviesByTerm(String term) {
         String url = String.format("https://api.themoviedb" +
-                ".org/3/search/movie?api_key=21bf4ea44600c1fd5d93194cbfd6fd71&query=%s", term);
+                ".org/3/search/movie?api_key=%s&query=%s", this.apiKey, term);
         return new JsonIO().load(url);
     }
 }
